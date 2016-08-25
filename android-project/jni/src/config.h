d72e6b0db9b236283a1915f5646b8f1002455430
jorgicor <jorge.giner@hotmail.com>
2016-08-25 18:57:27 +0200

    * Added android-project.

---
+++ android-project/jni/src/config.h
@@ -0,0 +1,39 @@
+/* Define to 1 if you have the <tgmath.h> header file. */
+/* #undef HAVE_TGMATH_H */
+
+/* Name of package */
+#define PACKAGE "planetvc"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "jorge.giner@hotmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Planet of the Vicious Creatures"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Planet of the Vicious Creatures 1.06"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "planetvc"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://jorgicor.sdfeu.org/planetvc"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.06"
+
+/* Enable cheats */
+/* #undef PP_DEBUG_ON */
+
+/* Demo version */
+/* #undef PP_DEMO_ON */
+
+/* Name of organization */
+#define PP_ORG_NAME "jorgicor"
+
+/* Use SDL to get the data folder instead of using DATADIR */
+#define PP_SDL_DATADIR_ON 1
+
+/* Version number of package */
+#define VERSION "1.06"
+
